f4d0d72bd9e0979b7214821c5274537c53902f3f
Allison Diego <80264145+AllisonDieg0@users.noreply.github.com>
2023-07-23 16:36:50 -0300

    Add files via upload

---
+++ desafio2/js/main.js
@@ -0,0 +1,9 @@
+const vasco = document.querySelector('#vasco')
+vasco.addEventListener("click", ()=>{
+    console.log("are reÊeeee vasco vai jogar a série b")
+})
+
+const des = document.querySelector('#calcular')
+des.addEventListener("click", function(){
+    console.log("fui clicado")
+})
